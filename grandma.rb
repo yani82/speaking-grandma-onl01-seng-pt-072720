84f045dab789d07ddb52e9e87d2e9a36ed30c399
Yani Asari <yani.iman1@gmail.com>
2020-07-30 12:06:40 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -3,7 +3,7 @@ def speak_to_grandma(phrase)
 if phrase != phrase .upcase
      return "HUH?! SPEAK UP, SONNY!"
       elsif phrase=="I LOVE YOU GRANDMA".upcase
-      return "I LOVE YOU TOO PUMPKIN" 
+      puts "I LOVE YOU TOO PUMPKIN" 
     else 
 return "NO, NOT SINCE 1938!"
     end
