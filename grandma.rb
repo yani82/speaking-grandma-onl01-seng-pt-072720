59d71725d49e0cf60c97611baa28adee72725949
Yani Asari <yani.iman1@gmail.com>
2020-07-30 11:32:46 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,5 +1,5 @@
 # Write a speak_to_grandma method.
-def speak_to_grandma(phrase = "")
+def speak_to_grandma(phrase = "Hi Nana, how are you?")
 if phrase != phrase.upcase
      return "HUH?! SPEAK UP, SONNY!"
       elsif phrase=="I LOVE YOU GRANDMA".upcase
