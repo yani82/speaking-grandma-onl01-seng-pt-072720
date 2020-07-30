c7dcbf8e4a2683dee6d50d3a1ab901b02fdb267c
Yani Asari <yani.iman1@gmail.com>
2020-07-30 13:03:18 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,6 +1,6 @@
 # Write a speak_to_grandma method.
 def speak_to_grandma(phrase)
-if phrase != "I love you grandma".upcase
+if phrase = "I love you grandma".upcase
      return "HUH?! SPEAK UP, SONNY!"
       elsif phrase==phrase.upcase
       return "NO, NOT SINCE 1938!" 
